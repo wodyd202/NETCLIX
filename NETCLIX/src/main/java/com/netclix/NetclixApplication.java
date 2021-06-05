aa0cfc0ef4298210bc627713a95d85c2c498251a
wodyd202 <wodyd202@naver.com>
2021-06-05 18:03:49 +0900

    project start

---
+++ NETCLIX/src/main/java/com/netclix/NetclixApplication.java
@@ -0,0 +1,13 @@
+package com.netclix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetclixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetclixApplication.class, args);
+	}
+
+}
